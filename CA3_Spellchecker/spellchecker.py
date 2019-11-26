a5a7ad008de0c1939b1ad291f1f2913bf8b98271
smileyowley <smileyowlie@gmail.com>
2019-11-26 23:22:04 +0000

    removing profanities, developed working prototype

--- CA3_Spellchecker/spellchecker.py
+++ CA3_Spellchecker/spellchecker.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 """
 module_title: 'Programming for Data Analytics'
 module_code: 'B9DA100'
@@ -8,22 +9,36 @@ assessment_title: 'Spellchecker'
 assessment_number: 'CA3'
 individual_or_group: 'Individual'
 issue_date: 13/11/2019
-submission_date: 24/11/2019
+submission_date: 1/12/2019
 mode_of_submission: 'Moodle/Github'
 student_name: 'Sophie Reisenleitner'
 student_number: 10544458
-github_link: 
+github_link: https://github.com/smileyowley/B9DA100-SR/tree/master/CA3_Spellchecker
+requirements: spell check a directory of files (eg: glob.glob functionality), add appropriate unittests to test the newly created function(s).
 """
 
+# SETTING THE WORKING DIRECTORY
+import os, glob
+
+print("**********************************************")
+print("* Welcome to the Spellchecker                *")
+print("* We will be checking the spelling of all    *")
+print("* files within the directory of your choice  *")
+print("**********************************************")
+print("")
+my_path = 'C:\1_DBS\1_Programming\B9DA100-SR\CA3_Spellchecker'
+# input("Please set your working directory here: ")
+
+# CREATING SPELLCHECKER OBJECT
 class SpellChecker(object):
     def __init__(self):
         self.words = []
 
     def load_file(self, file_name):
         my_file = open(file_name)
-        lines = my_file.readlines()
+        reading_lines = my_file.readlines()
         my_file.close()
-        return list(map(lambda x: x.strip().lower(), lines))
+        return list(map(lambda x: x.strip().lower(), reading_lines))
 
     def load_words(self, file_name):
         self.words = self.load_file(file_name)
@@ -35,7 +50,11 @@ class SpellChecker(object):
         for index, sentence in enumerate(self.sentences):
             failed_words_in_sentences.extend(self.check_words(sentence, index))
             index = index + 1
-        return failed_words_in_sentences
+        if not failed_words_in_sentences: # If there are no errors, print confirmation
+            print(file_name + " has no spelling errors.")
+        else: # If there are errors, print each word
+            return failed_words_in_sentences
+        
 
     def check_word(self, word):
         return word.strip('.').lower() in self.words
@@ -43,30 +62,26 @@ class SpellChecker(object):
     def check_words(self, sentence, index=0):
         words_to_check = sentence.split(' ')
         caret_position = 0
-        failed_words = []
+        failed_words = [] 
         for word in words_to_check:
             if not self.check_word(word):
-                print('Word is misspelt ' + word + ' at line : ' + str(index+1) + ' pos ' + str(caret_position+1))
+                print('Line ' + str(index+1) + ', position ' + str(caret_position+1) + ': "' + word.strip(".!,:-_/()") + '" is misspelt.')
                 failed_words.append({'word':word,'line':index+1,'pos':caret_position+1})
-            # update the caret position to be the length of the word plus 1 for the split character.
+            # updating the caret position to be the length of the word plus 1 for the split character.
             caret_position = caret_position + len(word) + 1
         return failed_words
 
+
+# CREATING AN INSTANCE OF THE SPELLCHECKER OBJECT
 if __name__ == '__main__':
     spellChecker = SpellChecker()
     spellChecker.load_words('spell.words')
-    print(len(spellChecker.words))
-    # now check if the word zygotic is a word
-    print(spellChecker.check_word('zygotic'))
-    print(spellChecker.check_word('zogotic'))
-    print(spellChecker.check_words('zygotic mistasdas elementary'))
-    print(spellChecker.check_document('darren.txt'))
+    print("Spellchecker's dictionary has " + str(len(spellChecker.words)) + " words.")
 
-    import os
-    import glob
-    os.chdir('R:/')
+# CHECKING THE WORDS IN ALL FILES WITHIN THE DIRECTORY
+    os.chdir(my_path)
     files = glob.glob('*.txt')
     for file in files:
-        print(file)
+        print("")
+        print("Checking " + file)
         spellChecker.check_document(file)
-        
--- CA3_Spellchecker/tester.py
+++ CA3_Spellchecker/tester.py
@@ -8,15 +8,17 @@ assessment_title: 'Spellchecker'
 assessment_number: 'CA3'
 individual_or_group: 'Individual'
 issue_date: 13/11/2019
-submission_date: 24/11/2019
+submission_date: 1/1/2019
 mode_of_submission: 'Moodle/Github'
 student_name: 'Sophie Reisenleitner'
 student_number: 10544458
-github_link: 
+github_link: https://github.com/smileyowley/B9DA100-SR/tree/master/CA3_Spellchecker
 """
 
 import unittest
-from spellcheck import SpellChecker
+from spellchecker import SpellChecker
+
+# CREATING TEST SPELLCHECKER OBJECT
 class TestSpellChecker(unittest.TestCase):
 
     def setUp(self):
@@ -24,23 +26,12 @@ class TestSpellChecker(unittest.TestCase):
         self.spellChecker.load_words('spell.words')
 
     def test_spell_checker(self):
-        self.assertTrue(len(self.spellChecker.words) > 50000)
-        self.assertTrue(self.spellChecker.check_word('zygotic'))
-        self.assertFalse(self.spellChecker.check_word('zogotic'))
-        self.assertEqual([{'line': 1, 'pos': 9, 'word': 'mistasdas'}],
-                self.spellChecker.check_words('zygotic mistasdas elementary'))
-        self.assertEqual([],
-                self.spellChecker.check_words('our first correct sentence'))
-        self.assertEqual(0,
-                len(self.spellChecker.check_words('Our first correct sentence.')))
-        self.assertEqual([{'line': 1, 'pos': 9, 'word': 'mistasdas'}, {'line': 1, 'pos': 19, 'word': 'spelllleeeing'}],
-                self.spellChecker.check_words('zygotic mistasdas spelllleeeing elementary'))
-        self.assertEqual(2,
-                len(self.spellChecker.check_words('zygotic mistasdas spelllleeeing elementary')))
-        #self.assertEqual(0, len(self.spellChecker.check_document('spell.words')))
-        self.assertEqual([{'line': 2, 'pos': 15, 'word': 'larn'}, {'line': 2, 'pos': 20, 'word': 'huw'}, {'line': 2, 'pos': 33, 'word': 'wurdz'}],
-                self.spellChecker.check_document('darren.txt'))
-
+        self.assertTrue(len(self.spellChecker.words) > 50000) #Checking the length of dictionary file
+        self.assertTrue(self.spellChecker.check_word('misspell'))
+        self.assertNotEqual([{'line': 1, 'pos': 13, 'word': 'the'}, {'line': 3, 'pos': 8, 'word': 'someone'}],
+                self.spellChecker.check_document('spelling_bad.txt'))
+        self.assertNotEqual([{'line': 3, 'pos': 1, 'word': 'whereas'}, {'line': 3, 'pos': 5, 'word': 'alright'}],
+                self.spellChecker.check_document('spelling_ok.txt'))
 
 if __name__ == '__main__':
     unittest.main()
